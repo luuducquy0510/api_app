26437017fa27af5902731afbc154eff9a63af0f1
luuducquy0510 <luuducquy05102003@gmail.com>
2025-03-13 23:38:19 +0900

    complete summarize tool

--- app/database.py
+++ app/database.py
@@ -1,18 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# Database connection settings
-DB_USER = "postgres"
-DB_PASSWORD = "postgres"
-DB_HOST = "localhost"  # Use "db" if connecting from another container
-DB_PORT = "5432"
-DB_NAME = "postgres"
 
 # Create the database URL
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
+DATABASE_URL = f"postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}"
+print(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
--- app/routes/authen.py
+++ app/routes/authen.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from .. import schemas, auth, models, database
-from sqlmodel import Session, select
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from app import schemas, auth, models, database
+from sqlmodel import Session
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 router = APIRouter(
     tags=["Authentication"],
-    prefix="/user"
 )
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 
 # @router.post("/create")
 def create_account(accounts: schemas.UserCreate):
@@ -16,7 +17,6 @@ def create_account(accounts: schemas.UserCreate):
     db_acc = models.User(
         email=accounts.email, 
         username=accounts.username, 
-        fullname=accounts.fullname, 
         password=hashed_password
     )
     with Session(database.engine) as session:
@@ -25,18 +25,13 @@ def create_account(accounts: schemas.UserCreate):
         session.refresh(db_acc)
     return schemas.UserResponse(
         username=db_acc.username,
-        fullname=db_acc.fullname
+        email = db_acc.email
     )
          
-
-def get_user_by_email(email: str):
-    with Session(database.engine) as session:
-        return session.exec(select(models.User).filter(models.User.email == email)).first()
-
 # Check if account already exist
 @router.post("/register", response_model=schemas.UserResponse)
 def register_account(account: schemas.UserCreate):
-    db_acc = get_user_by_email(account.email)
+    db_acc = auth.get_user_by_email(account.email)
     if db_acc:
         raise HTTPException(status_code=400, detail="Email already registered")
     return create_account(account)
@@ -44,7 +39,7 @@ def register_account(account: schemas.UserCreate):
 # Login (returns JWT token)
 @router.post("/login", response_model=schemas.Token)
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = get_user_by_email(form_data.username)
+    user = auth.get_user_by_email(form_data.username)
     
     if not user or not auth.verify_password(form_data.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
@@ -52,13 +47,4 @@ def login(form_data: OAuth2PasswordRequestForm = Depends()):
     access_token = auth.create_access_token(data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
 
-# Protected route (requires authentication)
-@router.get("/protected")
-def protected_route(token: str = Depends(oauth2_scheme)):
-    payload = auth.verify_token(token)
-    email = payload.get("sub")
-    user = get_user_by_email(email)
-    if not user:
-        raise HTTPException(status_code=401, detail="Invalid user")
-    
-    return {"message": f"Hello, {user.fullname}! You are authenticated."}
+
